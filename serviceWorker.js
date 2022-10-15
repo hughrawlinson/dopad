cc87b206e2e97b38e688692efa203bfdc5fb07e3
antoni-devlin <antoni.devlin@gmail.com>
2022-10-15 17:05:43 +0100

    Updated favicon, PWA-ified, and fixed initial-load bug

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticDopad = "dopad-site-v1"
+const assets = [
+	"/",
+	"/index.html",
+	"/main.css",
+	"/script.js",
+	"/assets/favicon.png"
+]
+
+self.addEventListener("install", installEvent => {
+	installEvent.waitUntil(
+		caches.open(staticDopad).then(cache => {
+			cache.addAll(assets)
+		})
+	)
+})
+
+self.addEventListener("fetch", fetchEvent => {
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then(res => {
+			return res || fetch(fetchEvent.request)
+		})
+	)
+})
