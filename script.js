091235bc5f0422ec2234915ec26e3634aadef1fe
Antoni <antoni-devlin@users.noreply.github.com>
2022-12-01 09:25:29 +0000

    Object storage fixes (#9)
    ## Changed how task objects are created for storage
    
    * Removed the Task() function for creating new task objects in Add Task code, replaced it with an object literal instead.
    
    * Removed Task() function code that was commented out.

--- script.js
+++ script.js
@@ -45,14 +45,6 @@ window.addEventListener("load", (e) => {
 	}
 });
 
-
-// Task object
-function Task(id, content) {
-	this.id = id;
-	this.content = content;
-	this.completed = false;
-}
-
 // Render task list on frontend
 function render(tasksArray) {
 	document.querySelectorAll('.task').forEach(task => {
@@ -110,19 +102,19 @@ document.getElementsByClassName('clear-tasks')[0].addEventListener('click', (e) 
 
 // Generate UUID
 function generateUUID() { // Public Domain/MIT
-    var d = new Date().getTime();//Timestamp
-    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16;//random number between 0 and 16
-        if(d > 0){//Use timestamp until depleted
-            r = (d + r)%16 | 0;
-            d = Math.floor(d/16);
-        } else {//Use microseconds since page-load if supported
-            r = (d2 + r)%16 | 0;
-            d2 = Math.floor(d2/16);
-        }
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
+	var d = new Date().getTime();//Timestamp
+	var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = Math.random() * 16;//random number between 0 and 16
+		if (d > 0) {//Use timestamp until depleted
+			r = (d + r) % 16 | 0;
+			d = Math.floor(d / 16);
+		} else {//Use microseconds since page-load if supported
+			r = (d2 + r) % 16 | 0;
+			d2 = Math.floor(d2 / 16);
+		}
+		return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+	});
 }
 
 // Add task on enter
@@ -139,7 +131,11 @@ newTaskField.addEventListener("keydown", (event) => {
 			}
 			tasks = JSON.parse(localStorage.getItem('tasks'))
 			// Create new task object to be stored
-			let newTask = new Task(generateUUID(), newTaskField.innerHTML)
+			const newTask = {
+				id: generateUUID(),
+				content: newTaskField.innerHTML,
+				completed: false
+			}
 			// Add new task object to end of JS array
 			tasks.push(newTask)
 			// Stringify tasks JS array, and write it back to localStorage
